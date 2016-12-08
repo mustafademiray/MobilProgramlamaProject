c60cd6dd925d39b1b37411e1748c4640471f711f
Eren Durmus <azaezel60@gmail.com>
2016-12-08 20:23:41 +0300

    Eren Durmuş commiti

--- app/src/main/java/com/example/mustafa/githubproject/MainActivity.java
+++ app/src/main/java/com/example/mustafa/githubproject/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.mustafa.githubproject;
+// EREN DURMUŞ 130202018
+//https://github.com/Azaezell
+//https://github.com/mustafademiray/MobilProgramlamaProject
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //EREN DURMUŞ 130202018
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
